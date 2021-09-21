923c41a895ef557d58169b3375110d2c7cb1d5e6
mosdefinite <84642826+mosdefinite@users.noreply.github.com>
2021-09-21 14:43:35 -0400

    Fixed Pet Class Attributes

--- VirtualPet/Pet.cs
+++ VirtualPet/Pet.cs
@@ -6,6 +6,51 @@ namespace VirtualPet
 {
     public class Pet
     {
-        public string Name;
+
+
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int Hunger { get; set; }
+        public int Boredom { get; set; }
+        public int Health { get; set; }
+
+        //public int Feed { get; set; }  Not property Should be method
+        //public int SeeDoctor { get; set; }
+        //public int Play { get; set; }
+
+        public Pet()
+        {
+            Hunger = 50;
+            Boredom = 50;
+            Health = 100;
+
+        }
+
+        public Pet(string name, string species)
+        {
+            Name = name;
+            Species = species;
+
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetSpecies(string species)
+        {
+            Species = species;
+        }
+
+        public string GetSpecies()
+        {
+            return Species;
+        }
     }
 }
