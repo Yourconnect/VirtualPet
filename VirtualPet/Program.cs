2f03172670ea57f4fc5694cb455e3db0e1e34205
mosdefinite <84642826+mosdefinite@users.noreply.github.com>
2021-09-22 13:38:49 -0400

    unfinished menu, to merge with dev to get variables/methods from pet

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -33,14 +33,15 @@ namespace VirtualPet
                         
                         break;
                     case "2":
-                        PrintStatus();
+                       // GetStatus();
                         
 
                         break;
                     case "3":
-
+                        PetInteractions();
                         break;
                     case "4":
+                        AdoptPet();
 
                         break;
                     case "5":
@@ -64,16 +65,76 @@ namespace VirtualPet
                 return new Pet(Species, Name);
             }   
             
-            static void PrintStatus()
-            {
-                Console.WriteLine("Hunger: "+ MyPet.Hunger);
-                Console.WriteLine("Boredom: "+ MyPet.Boredom);
-                Console.WriteLine("Health: "+ MyPet.Health);
-                Console.Read();
+            //static void PrintStatus()
+            //{
+
+            //    Console.Clear();
+            //    Console.WriteLine("Hunger: "+ MyPet.Hunger);
+            //    Console.WriteLine("Boredom: "+ MyPet.Boredom);
+            //    Console.WriteLine("Health: "+ MyPet.Health);
+            //    Console.Read();
 
             }
 
-            
+
+
+            static void AdoptPet()
+            {
+                Console.WriteLine("Would you like to adopt " + MyPet + "?");
+                Console.WriteLine("Yes or No?");
+
+                string choice = Console.ReadLine().ToLower();
+                if (choice == "yes")
+                {
+                    //MyPet.Remove();?? remove from pet list in shelter
+                    Console.WriteLine("Enjoy your new pet!");
+                }
+                else
+                {
+                    Console.WriteLine("Come back when you want to save a helpless animal");
+                }
+
+                Console.Read();
+                
+
+            }
+
+            static void PetInteractions()
+            {
+                Console.WriteLine("How would you like to interact with a pet today? ");
+                Console.WriteLine("Enter 1 to play with the pet \n Enter 2 to feed the pet \n Enter 3 to take the pet to the doctor");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                Console.WriteLine(MyPet.Name + " is " + MyPet.Boredom + "% bored");
+
+                //MyPet.Play;
+
+                Console.WriteLine(MyPet.Name + " is playing");
+
+                Console.WriteLine(MyPet.Name + "is now " + MyPet.Boredom + "% bored");
+
+            }
+            else if (choice == 2)
+            {
+                //MyPet.Feed;
+            }
+            else if (choice == 3)
+            {
+                //MyPet.Doctor
+
+                Console.WriteLine(");
+            }
+            else
+            {
+                Console.WriteLine("Please choose option 1, 2 or 3");
+            }
+
+
+               
+
+            }
            
         }
     }
