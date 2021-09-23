ca68349364026b561d43e9f1f182264085379d0e
Naziyah <ziyahfolmar@gmail.com>
2021-09-23 13:46:09 -0700

    added Shelter Tests

--- VirtualPet.Tests/ShelterTests.cs
+++ VirtualPet.Tests/ShelterTests.cs
@@ -8,7 +8,7 @@ namespace VirtualPet.Tests
     public class ShelterTests
     {
         [Fact]
-        public void Should_List_All_Pets_In_Shelter()
+        public void ListOfPets_Should_List_All_Pets_In_Shelter()
         {
             //Arrange
             Shelter shelter = new Shelter();
