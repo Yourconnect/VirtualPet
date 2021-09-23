92d9d858df06425d55603544ecec90738fd903aa
1Jhovany <cuevasjhovany@gmail.com>
2021-09-23 13:29:46 -0500

    Changes

---
+++ RobotTests/RobotTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace RobotTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public class RobotTests
+        {
+            private Robot testRobot;
+
+            public RobotTests()
+            {
+                testRobot = new Pet("Species", "Name");
+            }
+                public void Test1()
+        {
+
+        }
+    }
+}
